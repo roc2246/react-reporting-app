a3740ef1c0dde3a0f63dfd9a1b881f6aaa6f7e6e
Riley <riley.childs@yahoo.com>
2025-12-23 08:29:35 -0700

    Copied index file from previous vanilla project

--- server/index.js
+++ server/index.js
@@ -1,34 +1,87 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-// import routes from "./routes/index.js"; 
-import dotenv from "dotenv";
-// import { dbConnect } from "./models/index.js";
-
-dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const admin = require("firebase-admin");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const path = require("path");
 
-// await dbConnect();
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
-app.use(express.static(path.join(__dirname, "public")));
+const router = require("./routes/index");
+const middleware = require("./middleware/index");
+
+// Imports config files
+require("dotenv").config({
+  path: path.join(__dirname, "../config/.env"),
+});
+
+// Initialize Firebase admin
+(() => {
+  const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://reportingapp---file-uploads.firebaseio.com",
+    storageBucket: "reportingapp---file-uploads.appspot.com"
+  });
+})();
+
+// Parse JSON request bodies
 app.use(express.json());
 
-// app.use("/api", routes);
+// Parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res) => {
-  res.status(404).send("Not Found");
+// Configures express session
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), // Replace with your MongoDB connection string
+  })
+);
+
+// Apply the requireLogin middleware to protect the restricted page route
+app.get("/hours-and-fba.html", middleware.manageLogin, (req, res) => {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, private");
+  res.sendFile(__dirname + "/views/hours-and-fba.html");
 });
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: "Internal Server Error" });
+// Set up middleware for static files (CSS, JS, images, etc.)
+app.use(express.static("views"));
+
+// Sets up route for home page
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// Set up route for login page
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+
+
+// Sets up routers
+app.use("/", router);
+
+// Sets up recovery logic
+// require("./recovery/index")();
+
+// Sets up websocket functionality
+/* HOLD OFF ON WEBSOCKET FUNCTIONALITY */
+// require("./websocket/index")(app);
+
+// Sets up chron jobs
+require("./chron/index")();
+
+// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
