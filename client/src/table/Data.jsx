03605dbb795c8f971d9630fe1c40f619099d0fdf
Riley <riley.childs@yahoo.com>
2025-12-30 18:25:46 -0700

    Fixed logic for 7 day orders volume

--- client/src/table/Data.jsx
+++ client/src/table/Data.jsx
@@ -1,15 +1,23 @@
+import React from "react";
+
 export default function Data({ data, className, category }) {
-  if (!data || !Array.isArray(data)) return null;
+  if (!Array.isArray(data)) return null;
 
   return (
     <>
       {data.map((row, idx) => (
-        <td
-          key={idx}
-          className={`${className}__table-category-value--${category}`}
-        >
-          {category}
-        </td>
+        <React.Fragment key={idx}>
+          
+          <td className={`${className}__table-category-value--${category}`}>
+            {category}
+          </td>
+          {className === "_7-day-volumes-report" && (
+            <td className={`${className}__table-category-value--${category}`}>
+              {row[category] ?? "N/A"}
+            </td>
+          )}
+
+        </React.Fragment>
       ))}
     </>
   );
